763c72972685b96624d03aac673faf6661ee7cb2
danielalvarado <danialvarado@ic-itcr.ac.cr>
2018-03-26 23:40:12 -0600

    Some changes

--- apps/store/models.py
+++ apps/store/models.py
@@ -6,20 +6,24 @@ class Tipo_Usuario(models.Model):
 	descripcion = models.CharField(max_length = 100)
 
 class Usuario(models.Model):
-	idTipoUsuario = models.ForeignKey(Tipo_Usuario, on_delete = SET_NULL)
+	idTipoUsuario = models.ForeignKey(Tipo_Usuario, on_delete = models.CASCADE)
 	username = models.CharField(max_length = 20)
 	password = models.CharField(max_length = 30)
-	email = models.CharField(max_length = 30)
+	email = models.EmailField()
 	nombre = models.CharField(max_length = 50)
 	apellidos = models.CharField(max_length = 100)
+
+
+class Categoria(models.Model):
+	descripcion = models.CharField(max_length = 100)
 
 class Producto(models.Model):
 	nombre = models.CharField(max_length = 50)
 	descripcion = models.CharField(max_length = 250)
 	precio = models.IntegerField()
-	oferta = models.BooleanField(intial = False)
+	oferta = models.BooleanField()
 	codigo = models.IntegerField()
-	idCategoria = models.ForeignKey(Categoria, on_delete = models.SET_DEFAULT)
+	idCategoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
 
 class Carrito(models.Model):
 	idUsuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
@@ -29,16 +33,21 @@ class Carrito_Detalle(models.Model):
 	idProducto = models.ForeignKey(Producto, on_delete = models.CASCADE)
 	cantidad = models.IntegerField()
 
-class Categoria(models.Model):
-	descripcion = models.CharField(max_length = 100)
 
 
 class Historial(models.Model):
-	idUsuario = models.ForeignKey(Usuario, on_delete = SET_NULL)
+	idUsuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
 	fecha = models.DateField()
 
 
 class Historial_Detalle(models.Model):
-	idHistorial = models.ForeignKey(Historial, on_delete = SET_NULL)
-	idProducto = models.ForeignKey(Producto, on_delete = SET_NULL)
+	idHistorial = models.ForeignKey(Historial, on_delete = models.CASCADE)
+	idProducto = models.ForeignKey(Producto, on_delete = models.CASCADE)
 	cantidad = models.IntegerField()
+
+
+	'''
+models.CASCADE)
+Cascade deletes. Django emulates the behavior of the SQL 
+constraint ON DELETE CASCADE and also deletes the object containing the ForeignKey.
+	'''
