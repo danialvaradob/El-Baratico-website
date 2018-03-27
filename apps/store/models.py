7ed7c616b8f1aa3e883a17097eb60cd330536ccc
danielalvarado <danialvarado@ic-itcr.ac.cr>
2018-03-26 22:52:02 -0600

    Agregando los primeros modelos

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tipo_Usuario(models.Model):
+	descripcion = models.CharField(max_length = 100)
+
+class Usuario(models.Model):
+	idTipoUsuario = models.ForeignKey(Tipo_Usuario, on_delete = models.CASCADE)
+	username = models.CharField(max_length = 20)
+	password = models.CharField(max_length = 30)
+	email = models.CharField(max_length = 30)
+	nombre = models.CharField(max_length = 50)
+	apellidos = models.CharField(max_length = 100)
+
+class Carrito(models.Model):
+	
