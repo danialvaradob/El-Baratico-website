6f91f3f1098aaa266e99c02980d5076cdd75a9ae
rrsegnini <roberto.t.x@hotmail.com>
2018-04-01 01:42:13 -0600

    Some features added

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^login/$', views.login, name='login'), 
+    url(r'^login/$', views.login, name='login'),
+    path('<int:producto_id>/', views.detail, name='detail'),
+    path('<int:producto_id>/', views.DetailView.as_view(), name='detail'),
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$',views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
-    path('<int:producto_id>/', views.DetailView.as_view(), name='detail'),
     url(r'^login/$', views.login, name='login'),      
 ]
 
