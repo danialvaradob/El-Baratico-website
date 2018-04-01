ac06200deec85fac82008d97d52162cc04738e67
rrsegnini <roberto.t.x@hotmail.com>
2018-03-31 23:09:32 -0600

    Url fixes

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^login/$', views.login, name='login'), 
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$',views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
     path('<int:producto_id>/', views.DetailView.as_view(), name='detail'),
-    url(r'^login/$', auth_views.login, name='login'),      
+    url(r'^login/$', views.login, name='login'),      
 ]
 
 
